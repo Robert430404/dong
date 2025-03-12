47deb5709183fc93c5ae0e0323e4b5ae64cfe868
Robert Joseph Cox Jr <robert@studiorclv.com>
2025-03-11 17:08:29 -0700

    added basic handling for fail state

--- screens/gameScreen.lua
+++ screens/gameScreen.lua
@@ -14,16 +14,23 @@ local gameScreen = {
 		y = 0
 	},
 
-	ballVelocity = 1,
+	ballVelocity = {
+		x = 0,
+		y = 0
+	},
 	ball = {
-		x = 100,
-		y = 100,
+		x = 0,
+		y = 0,
 		height = 20,
-		width = 20
+		width = 20,
+		isMoving = false,
+		maxXVelocity = 15,
+		maxYVelocity = 15,
+		velocityCompoundFactor = 1.5
 	}
 }
 
-local maxVerticalOffset = systemConfig:getScreenHeight() - gameScreen.paddleHeight - gameScreen.paddleGap;
+local maxVerticalOffset = systemConfig:getScreenHeight() - gameScreen.paddleHeight - gameScreen.paddleGap
 
 -- Handle fullscreen shift and recalculate the math
 function gameScreen:handleFullscreen()
@@ -43,9 +50,6 @@ function gameScreen:handleFullscreen()
 	rightHorizontalOffset = systemConfig:getScreenWidth() - gameScreen.paddleWidth - gameScreen.paddleGap
 	rightVerticalOffset = (systemConfig:getScreenHeight() / 2) - (gameScreen.paddleHeight / 2)
 
-	ballHorizontalOffset = (systemConfig:getScreenWidth() / 2) - (gameScreen.ball.width / 2)
-	ballVerticalOffset = (systemConfig:getScreenHeight() / 2) - (gameScreen.ball.height / 2)
-
 	-- Set the game state
 	gameScreen.leftPaddle.x = leftHorizontalOffset
 	gameScreen.leftPaddle.y = leftVerticalOffset
@@ -53,15 +57,30 @@ function gameScreen:handleFullscreen()
 	gameScreen.rightPaddle.x = rightHorizontalOffset
 	gameScreen.rightPaddle.y = rightVerticalOffset
 
-	gameScreen.ball.x = ballHorizontalOffset
-	gameScreen.ball.y = ballVerticalOffset
+	self:centerBall()
 
 	-- Set the global
-	maxVerticalOffset = systemConfig:getScreenHeight() - gameScreen.paddleHeight - gameScreen.paddleGap;
+	maxVerticalOffset = systemConfig:getScreenHeight() - gameScreen.paddleHeight - gameScreen.paddleGap
+end
+
+function gameScreen:centerBall()
+	ballHorizontalOffset = (systemConfig:getScreenWidth() / 2) - (gameScreen.ball.width / 2)
+	ballVerticalOffset = (systemConfig:getScreenHeight() / 2) - (gameScreen.ball.height / 2)
+
+	gameScreen.ball.x = ballHorizontalOffset
+	gameScreen.ball.y = ballVerticalOffset
 end
 
 -- Render the screen
 function gameScreen:render()
+	if self.ball.isMoving then
+		self:calculateBallPosition()
+	end
+
+	if self:ballDidCollide() then
+		self:handleBallVelocity()
+	end
+
 	love.graphics.setColor( love.math.colorFromBytes(46, 207, 133) )
 	self:drawPaddle(self.leftPaddle.x, self.leftPaddle.y)
 	self:drawPaddle(self.rightPaddle.x, self.rightPaddle.y)
@@ -78,7 +97,88 @@ function gameScreen:drawBall(x, y)
 	love.graphics.rectangle( "fill", x, y, self.ball.width, self.ball.height )
 end
 
+function gameScreen:calculateBallPosition()
+	local computedX = self.ball.x + self.ballVelocity.x
+	self.ball.x = computedX
+
+	local computedY = self.ball.y + self.ballVelocity.y
+	self.ball.y = computedY
+end
+
+function gameScreen:ballDidCollide()
+	local leftPaddle = self.leftPaddle
+	local rightPaddle = self.rightPaddle
+	local ball = self.ball
+
+	-- Coordinates Collided Left Paddle
+	if ball.x <= (leftPaddle.x + self.paddleWidth) then
+		local isBelowPaddle = ball.y > (leftPaddle.y + self.paddleHeight)
+		local isAbovePaddle = ball.y + ball.height < leftPaddle.y
+		if isBelowPaddle or isAbovePaddle then
+			self:handleLoss("left")
+
+			return false
+		end
+
+		self.ball.x = leftPaddle.x + self.paddleWidth
+
+		return true
+	end
+
+	if (ball.x + self.ball.width) >= rightPaddle.x then
+		local isBelowPaddle = ball.y > (rightPaddle.y + self.paddleHeight)
+		local isAbovePaddle = ball.y + ball.height < rightPaddle.y
+		if isBelowPaddle or isAbovePaddle then
+			self:handleLoss("right")
+
+			return false
+		end
+
+		self.ball.x = rightPaddle.x -  self.ball.width
+
+		return true
+	end
+
+	return false
+end
+
+function gameScreen:handleBallVelocity()
+	local maxXVelocity = self.ball.maxXVelocity
+	local maxYVelocity = self.ball.maxYVelocity
+	local compoundFactor = self.ball.velocityCompoundFactor
+
+	local computedX = (self.ballVelocity.x * -1) * compoundFactor
+	if computedX <= (maxXVelocity * -1) or computedX >= maxXVelocity then
+		self.ballVelocity.x = self.ballVelocity.x * -1
+	else
+		self.ballVelocity.x = computedX
+	end
+
+	local computedY = (self.ballVelocity.y * -1) * compoundFactor
+	if computedY <= (maxYVelocity * -1) or computedY >= maxYVelocity then
+		self.ballVelocity.y = self.ballVelocity.y * -1
+	else
+		self.ballVelocity.y = computedY
+	end
+end
+
+function gameScreen:handleLoss(side)
+	self.ball.isMoving = false
+
+	self:centerBall()
+
+	self.ballVelocity.x = 0
+	self.ballVelocity.y = 0
+
+	print(side .. " lost the game")
+end
+
 function gameScreen:handleKeypresses(key, scancode, isrepeat)
+	if key == "space" then
+		self.ballVelocity.x = 1
+		self.ball.isMoving = true
+	end
+
 	if key == "w" then
 		local computed = self.leftPaddle.y - self.paddleVelocity
 
