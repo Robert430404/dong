e82f3340c2c84d129054e9bd6a722cc6502fc244
Robert Joseph Cox Jr <robert@studiorclv.com>
2025-05-24 19:40:18 -0700

    implemented basic ai for single player play

--- opponent.lua
+++ opponent.lua
@@ -7,7 +7,27 @@ function opponent:run(gameState)
 		return
 	end
 
-	print(gameState)
+	local ballUpperBound = gameState.ball.y
+	local ballLowerBound = gameState.ball.y + gameState.ball.height
+
+	local paddleUpperBound = gameState.rightPaddle.y
+	local paddleLowerBound = gameState.rightPaddle.y + gameState.paddleHeight
+
+	local isBallAbovePaddle = ballLowerBound < paddleUpperBound
+	local isBallUnderPaddle = ballUpperBound > paddleLowerBound
+
+	if isBallAbovePaddle then
+		local newPaddleY = gameState.rightPaddle.y - gameState.paddleVelocity
+		if newPaddleY < 8 then
+			gameState.rightPaddle.y = gameState.offsetGap
+		else
+			gameState.rightPaddle.y = newPaddleY
+		end
+	elseif isBallUnderPaddle then
+		gameState.rightPaddle.y = gameState.rightPaddle.y + gameState.paddleVelocity
+	else
+		gameState.rightPaddle.y = gameState.rightPaddle.y
+	end
 end
 
 function opponent:setStatus(isEnabled)
